c180acaadccae21fb876c491603ea0072122ceef
Lovis Rentsch <gpg@lovirent.eu>
2026-04-03 14:19:08 +0200

    packaged for nix

--- flake.nix
+++ flake.nix
@@ -17,6 +17,11 @@
         nix-gleam.overlays.default
       ];
     };
+    rebar3 =
+      pkgs.rebar3WithPlugins
+      {
+        plugins = with pkgs.beam28Packages; [pc];
+      };
   in {
     devShells.${system}.default = pkgs.mkShell {
       name = "gleam";
@@ -27,8 +32,32 @@
         rebar3
       ];
     };
-    packages.${system}.default = pkgs.buildGleamApplication {
-      src = ./.;
+    packages.${system} = rec {
+      default = pkgs.stdenv.mkDerivation {
+        name = "blog";
+        version = "0.1.0";
+        src = ./.;
+        buildInput = [ssg];
+        buildPhase = ''
+          ${ssg}/bin/blog_lovirent_eu
+
+        '';
+        installPhase = ''
+          mkdir -p $out
+          mv dist/* $out
+        '';
+      };
+      ssg = pkgs.buildGleamApplication {
+        REBAR_CACHE_DIR = "$TMP/.rebar-cache";
+        DEBUG = 1;
+        rebar3Package = rebar3;
+        buildInputs = [
+          pkgs.esbuild
+          pkgs.erlang_28
+          rebar3
+        ];
+        src = ./.;
+      };
     };
   };
 }
